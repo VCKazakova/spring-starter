56047021bfe52e8c75d5769bab15585e5175e9ab
VCKazakova <v.c.kazakova@gmail.com>
2022-06-12 13:53:17 +0300

    Initialization beans

---
+++ src/main/java/com/kazakova/spring/service/CompanyService.java
@@ -0,0 +1,11 @@
+package com.kazakova.spring.service;
+
+public class CompanyService {
+
+    private UserService userService;
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+}
