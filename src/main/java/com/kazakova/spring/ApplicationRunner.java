d40fcd5e9304e3ffa741007a83fa5470ed483870
VCKazakova <v.c.kazakova@gmail.com>
2022-06-12 15:54:39 +0300

    BFPP

--- src/main/java/com/kazakova/spring/ApplicationRunner.java
+++ src/main/java/com/kazakova/spring/ApplicationRunner.java
@@ -2,11 +2,20 @@ package com.kazakova.spring;
 
 import com.kazakova.spring.database.pool.ConnectionPool;
 import com.kazakova.spring.database.repository.CompanyRepository;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.Serializable;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
+        // проверка принадлежности бина классу и BeanFactoryPostProcessor через Reflection API
+        String value = "hello";
+        System.out.println(CharSequence.class.isAssignableFrom(value.getClass()));
+        System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
+        System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
+
         try (var context = new ClassPathXmlApplicationContext("application.xml")) {
             //      clazz -> String -> Map<String, Object>
             var connectionPool = context.getBean("p1", ConnectionPool.class);
