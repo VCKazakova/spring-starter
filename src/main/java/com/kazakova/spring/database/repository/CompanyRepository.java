cf15efd15e2bfb2fcb1e64717825d745da6745e9
VCKazakova <v.c.kazakova@gmail.com>
2022-06-18 16:00:27 +0300

    BPP

--- src/main/java/com/kazakova/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/kazakova/spring/database/repository/CompanyRepository.java
@@ -1,13 +1,34 @@
 package com.kazakova.spring.database.repository;
 
+import com.kazakova.spring.bpp.Auditing;
 import com.kazakova.spring.bpp.InjectBean;
+import com.kazakova.spring.bpp.Transaction;
+import com.kazakova.spring.database.entity.Company;
 import com.kazakova.spring.database.pool.ConnectionPool;
 
-public class CompanyRepository {
+import javax.annotation.PostConstruct;
+import java.util.Optional;
+
+@Transaction
+@Auditing
+public class CompanyRepository implements CrudRepository<Integer, Company> {
 
     @InjectBean
     private final ConnectionPool connectionPool;
 
+    @PostConstruct
+    public void init() {
+        System.out.println("Init company repo");
+    }
 
+    @Override
+    public Optional<Company> findById(Integer id) {
+        System.out.println("Find by id method");
+        return Optional.of(new Company(id));
+    }
 
+    @Override
+    public void delete(Company entity) {
+        System.out.println("Delete method");
+    }
 }
