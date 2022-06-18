dc3d5981f6e2b221f0d25a253772473b42f64d8d
VCKazakova <v.c.kazakova@gmail.com>
2022-06-18 17:00:33 +0300

    Component scan

--- src/main/java/com/kazakova/spring/service/UserService.java
+++ src/main/java/com/kazakova/spring/service/UserService.java
@@ -1,21 +1,20 @@
 package com.kazakova.spring.service;
 
-import com.kazakova.spring.database.repository.CompanyRepository;
+import com.kazakova.spring.database.entity.Company;
+import com.kazakova.spring.database.repository.CrudRepository;
 import com.kazakova.spring.database.repository.UserRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
 
     private final UserRepository userRepository;
-    private final CompanyRepository companyRepository;
-    private CompanyService companyService;
+    private final CrudRepository<Integer, Company> companyRepository;
 
     public UserService(UserRepository userRepository,
-                       CompanyRepository companyRepository) {
+                       CrudRepository<Integer, Company> companyRepository) {
         this.userRepository = userRepository;
         this.companyRepository = companyRepository;
     }
 
-    public void setCompanyService(CompanyService companyService) {
-        this.companyService = companyService;
-    }
 }
