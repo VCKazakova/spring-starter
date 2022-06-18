9d103c8dd1fe1576112971c3158a30f95420f7bb
VCKazakova <v.c.kazakova@gmail.com>
2022-06-18 17:19:31 +0300

    Java configuration

---
+++ src/main/java/com/kazakova/spring/config/ApplicationConfiguration.java
@@ -0,0 +1,20 @@
+package com.kazakova.spring.config;
+
+import com.kazakova.spring.database.repository.CrudRepository;
+import com.kazakova.web.config.WebConfiguration;
+import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.stereotype.Component;
+
+//@ImportResource("classpath:application.xml")
+@Import(WebConfiguration.class)
+@Configuration
+@PropertySource("classpath:application.properties")
+@ComponentScan(basePackages = "com.kazakova.spring",
+        useDefaultFilters = false, includeFilters = {
+        @Filter(type = FilterType.ANNOTATION, value = Component.class),
+        @Filter(type = FilterType.ASSIGNABLE_TYPE, value = CrudRepository.class),
+        @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository")
+})
+public class ApplicationConfiguration {
+}
---
+++ src/main/java/com/kazakova/web/config/WebConfiguration.java
@@ -0,0 +1,10 @@
+package com.kazakova.web.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfiguration {
+
+
+
+}
