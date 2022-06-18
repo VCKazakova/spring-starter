48962844cbf603cce51873f37ca412d43260bc3c
VCKazakova <v.c.kazakova@gmail.com>
2022-06-18 16:16:28 +0300

    @Autowired

--- src/main/java/com/kazakova/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/kazakova/spring/database/repository/CompanyRepository.java
@@ -1,20 +1,30 @@
 package com.kazakova.spring.database.repository;
 
 import com.kazakova.spring.bpp.Auditing;
-import com.kazakova.spring.bpp.InjectBean;
 import com.kazakova.spring.bpp.Transaction;
 import com.kazakova.spring.database.entity.Company;
 import com.kazakova.spring.database.pool.ConnectionPool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import java.util.List;
 import java.util.Optional;
 
 @Transaction
 @Auditing
 public class CompanyRepository implements CrudRepository<Integer, Company> {
 
-    @InjectBean
-    private final ConnectionPool connectionPool;
+//    @Resource(name = "pool1")
+    @Autowired
+//    @Qualifier("pool1")
+    private ConnectionPool pool1;
+    @Autowired
+    private List<ConnectionPool> pools;
+    @Value("${db.pool.size}")
+    private Integer poolSize;
 
     @PostConstruct
     public void init() {
